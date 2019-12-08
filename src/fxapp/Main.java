9506a804c40544e5aa12150b8fe828dfbcf40dc3
pietrzakkuba <pietrzakkuba@yahoo.pl>
2019-12-08 21:16:35 +0100

    basic main menu

--- src/fxapp/Main.java
+++ src/fxapp/Main.java
@@ -2,7 +2,6 @@ package fxapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,10 +9,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("main-menu.fxml"))));
+        primaryStage.setTitle("Library");
         primaryStage.show();
+
     }
 
 
