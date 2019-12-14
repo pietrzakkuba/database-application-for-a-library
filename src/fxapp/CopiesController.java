a35d5b5f03f4327137a85636ec89b87d15e892f1
pietrzakkuba <pietrzakkuba@yahoo.pl>
2019-12-14 18:45:24 +0100

    controllers added

---
+++ src/fxapp/CopiesController.java
@@ -0,0 +1,23 @@
+package fxapp;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class CopiesController {
+    public Button toMainMenuButton;
+
+    public void toMainMenu(ActionEvent actionEvent) {
+        Platform.runLater( () -> {
+            try {
+                Main.getMainStage().setScene(new Scene(FXMLLoader.load(getClass().getResource("main-menu.fxml"))));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
